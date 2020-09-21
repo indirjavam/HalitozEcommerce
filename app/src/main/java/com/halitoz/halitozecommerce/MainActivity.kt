c7614c99f9ec22e13ff484732392f0faec8ffd23
indirjavam <indirjava@gmail.com>
2020-09-21 20:02:40 +0300

    Creating a Popup Dialog / Alert Box

--- app/src/main/java/com/halitoz/halitozecommerce/MainActivity.kt
+++ app/src/main/java/com/halitoz/halitozecommerce/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.halitoz.halitozecommerce
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
-import android.view.Menu
-import android.view.MenuItem
 import com.halitoz.halitozecommerce.model.Product
-
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -23,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         for (i in 0..100){
 //            products.add(Product("Organic Apple", "http://via.placeholder.com/350/ffff00/ff0000",1.99))
-            products.add(Product("Organic Apple #$i", "http://via.placeholder.com/350/dddddd/000000",1.99))
+            products.add(Product("Dress Shirt #$i", "http://via.placeholder.com/350/dddddd/000000",1.99))
         }
 
         recycler_view.apply {
--- app/src/main/java/com/halitoz/halitozecommerce/ProductDetail.kt
+++ app/src/main/java/com/halitoz/halitozecommerce/ProductDetail.kt
@@ -1,7 +1,8 @@
 package com.halitoz.halitozecommerce
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.product_detail.*
 
 class ProductDetail : AppCompatActivity() {
@@ -12,5 +13,21 @@ class ProductDetail : AppCompatActivity() {
 
         val title = intent.getStringExtra("title")
         product_name.text = title
+
+        availability.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setMessage("Hey, $title is in stock!")
+                .setPositiveButton("OK") { dialog, which -> }
+                .create()
+                .show()
+        }
+//        Secenek -2
+    /*    availability.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setMessage("It's in stock!")
+                .setPositiveButton("OK") { dialog, which -> }
+                .create()
+                .show()
+        }*/
     }
 }
