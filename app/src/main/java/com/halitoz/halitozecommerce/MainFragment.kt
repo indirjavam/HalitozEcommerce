e9c724bcf203bebafa9ff8e1501e8f172e580f00
indirjavam <indirjava@gmail.com>
2020-09-24 17:31:35 +0300

    Setting up Gson to parse JSON for a RecyclerView, Gson Kütuphanesi pars etme operasyonu

--- app/src/main/java/com/halitoz/halitozecommerce/MainFragment.kt
+++ app/src/main/java/com/halitoz/halitozecommerce/MainFragment.kt
@@ -3,29 +3,46 @@ package com.halitoz.halitozecommerce
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
+import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.gson.Gson
 import com.halitoz.halitozecommerce.model.Product
 import kotlinx.android.synthetic.main.fragment_main.view.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 class MainFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_main,container,false)
 
-        val products = arrayListOf<Product>()
+/*        val products = arrayListOf<Product>()
 
         for (i in 0..100){
 //            products.add(Product("Organic Apple", "http://via.placeholder.com/350/ffff00/ff0000",1.99))
             products.add(Product("Dress Shirt #$i", "http://via.placeholder.com/350/dddddd/000000",1.99))
+        }*/
+
+
+        doAsync {
+            val json = URL("https://finepointmobile.com/data/products.json").readText()
+
+            uiThread {
+                d("daniel","json: $json")
+                val products = Gson().fromJson(json,Array<Product>::class.java).toList()
+
+                root.recycler_view.apply {
+                    //            layoutManager = LinearLayoutManager(this@MainActivity)
+                    layoutManager = GridLayoutManager(activity,2)
+                    adapter = ProductsAdapter(products)
+                }
+            }
         }
 
-        root.recycler_view.apply {
-            //            layoutManager = LinearLayoutManager(this@MainActivity)
-            layoutManager = GridLayoutManager(activity,2)
-            adapter = ProductsAdapter(products)
-        }
+
 
         return root
     }
--- app/src/main/java/com/halitoz/halitozecommerce/model/Product.kt
+++ app/src/main/java/com/halitoz/halitozecommerce/model/Product.kt
@@ -1,7 +1,14 @@
 package com.halitoz.halitozecommerce.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Product (
+
+    @SerializedName("name")
     val title: String,
+
+    @SerializedName("photo_url")
     val photoUrl: String,
+
     val price: Double
 )
